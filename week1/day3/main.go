cbe40783769972d0e6ffd63f51a64cab3dad3b4a
olivia <oliviachowdhury2005@gmail.com>
2026-06-20 21:22:43 +0530

    feat: add main.go demonstrating basic Go data types, type conversion, constants, iota, and string manipulation

--- week1/day3/main.go
+++ week1/day3/main.go
@@ -1,41 +1,87 @@
 package main
 
-import "fmt"
-
-const (
-	Sunday = iota
-	Monday
-	Tuesday
-	Wednesday
-	Thursday
-	Friday
-	Saturday
+import (
+	"fmt"
+	"strings"
 )
 
 func main() {
-	var name string = "Bob"
-	age := 30
+	name := "Olivia"
 
-	score := 95
-	average := float64(score)
+	if name == "Olivia" {
+		fmt.Println(name + " is a good girl")
+	}
 
-	var active bool
+	//that was short declaration using :=, lets use var and get to know about nasic data types
 
-	msg := fmt.Sprintf(
-		"%s is %d years old. Score: %.1f",
-		name,
-		age,
-		average,
+	var age int
+	age = 20
+
+	fmt.Println(age)
+	var b bool = true
+	fmt.Println(b)
+	var x float64 = 10.25
+	fmt.Println(x)
+	var s string = "Olivia"
+	fmt.Println(s)
+
+	//shunyaaataaaaaaaaaaa ok
+
+	var z float64
+	var k int
+	fmt.Println(z)
+	fmt.Println(k)
+	fmt.Println("zero value of bool:", b)
+	fmt.Println("zero value of string:", s)
+
+	//go doesnt allow type conversions the way python does :3
+	num1 := 21
+	fmt.Println(num1)
+	fmt.Println(float64(num1))
+
+	var o int
+	o = 9
+	y := 2.5
+	fmt.Println(float64(o) + y) //hmmm..besh shob type ek lagbe taholei hobe
+
+	const pi = 3.14159
+	var r float64
+	r = 10
+	fmt.Println("Area is ", pi*r*r)
+
+	//iota concept, completely new to me :D
+	// by default, iota increases by 1. so multiplying by 2 increases by 2
+	// multiplying iota by any 'n' increases counter by n
+	// adding any 'x' to iota increases counter by x. can set starting index
+	// using this added offset.
+
+	const (
+		Monday = (iota * 2) + 10
+		Tuesday
+		Wednesday
+		Thursday
+		Friday
+		Saturday
+		Sunday
 	)
 
-	fmt.Println(msg)
-	fmt.Println("Friday =", Friday)
-	fmt.Println("Zero bool =", active)
+	fmt.Println(Saturday)
+	fmt.Println(Sunday)
 
-	if age > 18 {
-		fmt.Println("Adult")
-	} else {
-		fmt.Println("Minor")
-	}
+	//String manipulation~ upper case conv, len, concat.
+
+	var myStr string = "I love mohiner ghoraguli"
+	fmt.Println(myStr)
+	fmt.Println(strings.ToUpper(myStr))
+
+	myStr2 := "Ghore Pherar Gaan soothes"
+
+	fmt.Println(len(myStr))
+	fmt.Println(myStr + " <3 " + myStr2)
+
+	message := fmt.Sprintf("My name is %s and I am %d years old", name, age)
+	fmt.Println(message)
+
+	//feeling complete now, can sleep peacefully :p
 
 }
